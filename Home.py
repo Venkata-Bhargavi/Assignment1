a7b8806941f34e2c9c3f69abcd40e88300c81399
Venkata-Bhargavi <sikhakolli.v@northeastern.edu>
2023-02-07 19:58:55 -0500

    removed st.setpage

--- Home.py
+++ Home.py
@@ -3,9 +3,9 @@ import streamlit as st
 st.sidebar.markdown("Home")
 st.markdown("Main Page")
 
-st.set_page_config(  # Alternate names: setup_page, page, layout
-    layout="wide",  # Can be "centered" or "wide". In the future also "dashboard", etc.
-    initial_sidebar_state="auto",  # Can be "auto", "expanded", "collapsed"
-    page_title='Venkata_Bhargavi_Sikhakolli',  # String or None. Strings get appended with "• Streamlit".
-    page_icon= None,  # String, anything supported by st.image, or None.
-)
+# st.set_page_config(  # Alternate names: setup_page, page, layout
+#     layout="wide",  # Can be "centered" or "wide". In the future also "dashboard", etc.
+#     initial_sidebar_state="auto",  # Can be "auto", "expanded", "collapsed"
+#     page_title='Venkata_Bhargavi_Sikhakolli',  # String or None. Strings get appended with "• Streamlit".
+#     page_icon= None,  # String, anything supported by st.image, or None.
+# )
